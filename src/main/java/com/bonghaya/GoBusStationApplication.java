0f2d1fedc6708cbf8cd435096033851da424d0ae
bong-ha Hwang <bonghaya@gmail.com>
2024-10-20 22:49:21 +0900

    spring dev setting

---
+++ src/main/java/com/bonghaya/GoBusStationApplication.java
@@ -0,0 +1,13 @@
+package com.bonghaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBusStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoBusStationApplication.class, args);
+	}
+
+}
